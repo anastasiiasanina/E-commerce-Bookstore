495a69cb22fab061a61a155ffc7921b29f6bb94a
julikss <julie.lehenka@gmail.com>
2023-06-15 23:52:12 +0300

    Add filter options for catalog

--- frontend/js/catalog-page.js
+++ frontend/js/catalog-page.js
@@ -1,41 +1,72 @@
-const rangeInput = document.querySelectorAll(".range-input input"),
-  priceInput = document.querySelectorAll(".price-input input"),
-  range = document.querySelector(".slider .progress");
-let priceGap = 10;
+//elements of price slider
+const rangeInput = document.querySelectorAll('.range-input input');
+const priceInput = document.querySelectorAll('.price-input input');
+const range = document.querySelector('.slider .progress');
 
-priceInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let minPrice = parseInt(priceInput[0].value),
-      maxPrice = parseInt(priceInput[1].value);
+//filter options
+const titlesOfGenres = document.getElementsByClassName('title');
+const titlesOfAuthors = document.getElementsByClassName('author');
 
-    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
-      if (e.target.className === "input-min") {
-        rangeInput[0].value = minPrice;
-        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-      } else {
-        rangeInput[1].value = maxPrice;
-        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-      }
-    }
-  });
-});
+//button 'find books'
+const filterBtn = document.querySelector('.find-btn');
 
-rangeInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let minVal = parseInt(rangeInput[0].value),
-      maxVal = parseInt(rangeInput[1].value);
+//chosen filter options
+const filters = {
+  genres: [], 
+  authors: [],
+  price: []
+}
 
-    if (maxVal - minVal < priceGap) {
-      if (e.target.className === "range-min") {
-        rangeInput[0].value = maxVal - priceGap;
-      } else {
-        rangeInput[1].value = minVal + priceGap;
-      }
+const handleClick = (e, arr) => {
+  const selectedText = e.target.textContent;
+
+  if (e.target.classList.contains('active')) {
+    e.target.classList.remove('active');
+    let index = arr.indexOf(selectedText);
+    arr.splice(index, 1);
+  } else {
+    e.target.classList.add('active');
+    arr.push(selectedText);
+  }
+}
+
+const handlePriceSlider = (e) => {
+  let priceGap = 10;
+  let minVal = parseInt(rangeInput[0].value);
+  let maxVal = parseInt(rangeInput[1].value);
+
+  if (maxVal - minVal < priceGap) {
+    if (e.target.className === 'range-min') {
+      rangeInput[0].value = maxVal - priceGap;
     } else {
-      priceInput[0].value = minVal;
-      priceInput[1].value = maxVal;
-      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+      rangeInput[1].value = minVal + priceGap;
     }
-  });
-});
+  } else {
+    priceInput[0].value = minVal;
+    priceInput[1].value = maxVal;
+    range.style.left = (minVal / rangeInput[0].max) * 100 + '%';
+    range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + '%';
+  }
+}
+
+const getFilters = () => {
+  let minPrice = priceInput[0].value;
+  let maxPrice = priceInput[1].value;
+  filters.price = [minPrice, maxPrice];
+  console.log(filters);
+  return filters;
+}
+
+for (const title of titlesOfGenres) {
+  title.addEventListener('click', (e) => handleClick(e, filters.genres));
+}
+
+for (const title of titlesOfAuthors) {
+  title.addEventListener('click', (e) => handleClick(e, filters.authors));
+}
+
+for (const input of rangeInput) {
+  input.addEventListener('input', handlePriceSlider);
+}
+
+filterBtn.addEventListener('click', getFilters)
