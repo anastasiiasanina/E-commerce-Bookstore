6e5989483eb437c2c5df36222ddaf689d7ad6dfc
julikss <julie.lehenka@gmail.com>
2023-06-20 01:41:54 +0300

    Add showing filtered books
    Function for showing books needs to be improved -
    prices have to be included too.

--- frontend/js/catalog-page.js
+++ frontend/js/catalog-page.js
@@ -3,12 +3,18 @@ const rangeInput = document.querySelectorAll(".range-input input");
 const priceInput = document.querySelectorAll(".price-input input");
 const range = document.querySelector(".slider .progress");
 
-//filter options
-const titlesOfGenres = document.getElementsByClassName("title");
-const titlesOfAuthors = document.getElementsByClassName("author");
-
 //button 'find books'
 const filterBtn = document.querySelector(".find-btn");
+
+//titles of the menu
+const titlesOfGenres = document.getElementsByClassName('title');
+const titlesOfAuthors = document.getElementsByClassName('author');
+
+//elements for showing books
+const bookListTemplate = document.querySelector('[books-list-template]');
+const bookListContainer = document.querySelector('[data-books-list-container]');
+const mainTitle = document.querySelector('[changing-title]');
+
 const searchInput = document.querySelector(".searchInput");
 const resultBox = document.querySelector(".resultBox");
 
@@ -19,19 +25,34 @@ const filters = {
   price: [],
 };
 
+const getBooksFromBD = async () => {
+  try {
+    let response = await fetch('http://localhost:8000/api/v1/books')
+    let data = await response.json();
+    let arrayOfBooks = [];
+    for (const book of data) {
+      arrayOfBooks.push(book)
+    }
+    return arrayOfBooks;
+  } catch (e) {
+    console.log('Error occured:', e);
+  }
+}
+
+//event handler for clicks on menu
 const handleClick = (e, arr) => {
   const selectedText = e.target.textContent;
-
-  if (e.target.classList.contains("active")) {
-    e.target.classList.remove("active");
+  if (e.target.classList.contains('active')) {
+    e.target.classList.remove('active');
     let index = arr.indexOf(selectedText);
     arr.splice(index, 1);
   } else {
-    e.target.classList.add("active");
+    e.target.classList.add('active');
     arr.push(selectedText);
   }
 };
 
+//event handler for price input
 const handlePriceSlider = (e) => {
   let priceGap = 10;
   let minVal = parseInt(rangeInput[0].value);
@@ -51,51 +72,103 @@ const handlePriceSlider = (e) => {
   }
 };
 
+//getting filter values
 const getFilters = () => {
   let minPrice = priceInput[0].value;
   let maxPrice = priceInput[1].value;
   filters.price = [minPrice, maxPrice];
-  console.log(filters);
   mainTitle.textContent = 'Found Books';
   return filters;
 };
 
-for (const title of titlesOfGenres) {
-  title.addEventListener("click", (e) => handleClick(e, filters.genres));
-}
+//filtering books
+const filterBooks = (e, arrayOfBooks) => {
+  getFilters();
+  const { genres, authors, price } = filters;
+  const filteredBooks = arrayOfBooks.filter(book => {
+    return (
+      genres.includes(book.genre) && authors.includes(book.author) 
+    );
+  });
+  
+  while (bookListContainer.firstChild) {
+    bookListContainer.removeChild(bookListContainer.firstChild);
+  }
 
-for (const title of titlesOfAuthors) {
-  title.addEventListener("click", (e) => handleClick(e, filters.authors));
-}
-
-for (const input of rangeInput) {
-  input.addEventListener("input", handlePriceSlider);
-}
-
-filterBtn.addEventListener('click', getFilters);
-
+  for (const book of filteredBooks) {
+    displayCard(book);
+  }
+} 
 
 //showing books
-const bookListTemplate = document.querySelector('[books-list-template]');
-const bookListContainer = document.querySelector('[data-books-list-container]');
-let bookResults = [];
-let bd = [];
+const displayCard = (bookInfo) => {
+  const card = bookListTemplate.content.cloneNode(true).children[0];
+  const bookNameHeader = card.querySelector('[data-name-book]');
+  const bookAuthorHeader = card.querySelector('[data-author-book]');
+  const bookPriceHeader = card.querySelector('[data-price-book]');
+  bookNameHeader.textContent = bookInfo.name;
+  bookAuthorHeader.textContent = bookInfo.author;
+  bookPriceHeader.textContent = bookInfo.price;
+  bookListContainer.append(card);
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-      bd.map((book) => {
-      const card = bookListTemplate.content.cloneNode(true).children[0];
-      const bookNameHeader = card.querySelector("[data-name-book]");
-      const bookAuthorHeader = card.querySelector("[data-author-book]");
-      const bookPriceHeader = card.querySelector("[data-price-book]");
-      bookNameHeader.textContent = book.name;
-      bookAuthorHeader.textContent = book.author;
-      bookPriceHeader.textContent = book.price;
-      bookListContainer.append(card);
-    });
-})
+//displaying titles on menu
+const displayCategories = (arrayOfBooks) => {
+  const genresContainer = document.querySelector('.categories-text');
+  const authorsContainer = document.querySelector('.authors-list');
+  for (const book of arrayOfBooks) {
+      const genre = document.createElement('div');
+      genre.classList.add('title');
+      genre.textContent = book.genre;
+      genresContainer.appendChild(genre);
+
+      const author = document.createElement('li');
+      author.classList.add('author');
+      author.textContent = book.author;
+      authorsContainer.appendChild(author);
+  }
+}
+
+//event handlers for clicks on categories
+const handleGenresTitleClick = (e) => {
+  handleClick(e, filters.genres);
+};
+
+const handleAuthorsTitleClick = (e) => {
+  handleClick(e, filters.authors);
+};
+
+const handleFilterButtonClick = (e) => {
+  filterBooks(e, allBooks);
+};
 
 
-filterBtn.addEventListener("click", getFilters);
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const allBooks = await getBooksFromBD();
+    displayCategories(allBooks);
+
+    for (const book of allBooks) {
+      displayCard(book);
+    }
+
+    for (const title of titlesOfGenres) {
+      title.addEventListener("click", handleGenresTitleClick);
+    }
+
+    for (const title of titlesOfAuthors) {
+      title.addEventListener("click", handleAuthorsTitleClick);
+    }
+
+    for (const input of rangeInput) {
+      input.addEventListener("input", handlePriceSlider);
+    }
+
+    filterBtn.addEventListener('click', handleFilterButtonClick);
+  } catch (error) {
+    console.log('Error occurred:', error);
+  }
+});
 
 
 //DropBox
