bfe6c93a007aecae46a6e5fffa454a26a65a48e7
Liubov Kvasova <bubochka102003@gmail.com>
2023-06-21 22:42:33 +0300

    Implemented cart API
    Implemented cart API using cookies

---
+++ backend/API/cart/cartController.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const cartHelper = require('./cartHelper');
+const {
+  CART_CREATE_MESSAGE,
+  CART_REMOVE_MESSAGE,
+  ADDED_TO_CART_MESSAGE,
+  REMOVED_FROM_CART_MESSAGE,
+  STATUS_OK
+} = require('./cartResources');
+
+const createCart = (req, res) => {
+  cartHelper.createCart(res.cookie);
+
+  res.send({
+    status: STATUS_OK,
+    message: CART_CREATE_MESSAGE
+  });
+};
+
+const removeCart = (req, res) => {
+  cartHelper.removeCart(res.clearCookie);
+
+  res.send({
+    status: STATUS_OK,
+    message: CART_REMOVE_MESSAGE
+  });
+};
+
+const addToCart = (req, res) => {
+  const { productId, quantity } = req.body;
+
+  const responseObject = {
+    status: STATUS_OK,
+    message: ADDED_TO_CART_MESSAGE
+  };
+
+  if (quantity === 0) {
+    cartHelper.removeFromCart(res.cookie, req.cookies, productId);
+    responseObject.message = REMOVED_FROM_CART_MESSAGE;
+  } else {
+    cartHelper.addToCart(res.cookie, req.cookies, { productId, quantity });
+  }
+
+  res.send(responseObject);
+};
+
+const removeFromCart = (req, res) => {
+  const { productId } = req.body;
+
+  cartHelper.removeFromCart(res.cookie, req.cookies, productId);
+
+  res.send({
+    status: STATUS_OK,
+    message: REMOVED_FROM_CART_MESSAGE
+  });
+};
+
+module.exports = {
+  createCart,
+  removeCart,
+  addToCart,
+  removeFromCart
+};
