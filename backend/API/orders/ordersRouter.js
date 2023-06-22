f4d1c66f85d74363dfb1ffd5a57b747beb933eed
ukioxz <ikatember@gmail.com>
2023-06-22 12:11:33 +0300

    Add gitignore

--- backend/API/orders/ordersRouter.js
+++ backend/API/orders/ordersRouter.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
-const controller = require('./ordersContoller');
+const controller = require("./ordersController");
 
-router.post('/api/v1/orders', controller.addOrder);
-router.get('/api/v1/orders', controller.getAllOrders);
-router.get('/api/v1/orders/:id', controller.getOrder);
-router.delete('/api/v1/orders/:id', controller.deleteOrder);
+router.post("/api/v1/orders", controller.addOrder);
+router.get("/api/v1/orders", controller.getAllOrders);
+router.get("/api/v1/orders/:id", controller.getOrder);
+router.delete("/api/v1/orders/:id", controller.deleteOrder);
 
 module.exports = router;
